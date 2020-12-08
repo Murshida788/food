b62548676a57fcf984f94c4192ae2b073ba43bde
Murshida788 <murshidaakter40@gmail.com>
2020-12-07 21:35:15 -0800

    first commit

---
+++ about.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+
+	
+	<link rel="stylesheet" href="food.css"/>
+</head>
+<body>
+	<header>
+		<h2 class="title"  style="color:#007bff; padding: 10px;"></h2>
+	
+      <h1><marquee>WELCOME TO FOOD CORNER</marquee></h1>
+</header>
+<div style="width: 30%;margin: 0 auto">
+<div class="des">
+   
+Our web page is basically a e-commarce site.An online platform where you can find and  order delicious foods within reasonable price.We will serve you with our best.
+<br>
+If you will face any trouble in our web page or delivery.please contact with:
+<br>
+Phone:01876524521
+<br>
+<br>
+<br>
+<br>
+<br>
+<h1>THANK YOU </h1>
+
+<div>
+</body>
